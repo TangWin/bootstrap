10fbd26e5a4bc2d64d46e1faa33e398af659f949
Tangw <17375770327@163.com>
2019-06-03 02:29:20 +0800

    bootstrap: tangw - init check.

--- bootstrap-start/src/main/java/com/tangw/bootstrap/controller/MainController.java
+++ bootstrap-start/src/main/java/com/tangw/bootstrap/controller/MainController.java
@@ -1,9 +1,22 @@
 package com.tangw.bootstrap.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
+ * 健康检查
  * @author tangw
  * @date 2019/6/3
  **/
 @RestController
+@RequestMapping(value = "")
 public class MainController {
+
+    @GetMapping("/check")
+    public String checkState() {
+        return "hello world!";
+    }
+
+
 }
